f80e17e11ff1e8e7a5635e214a058fda8bbf53a4
Rishi Giri <rushforlinux@gmail.com>
2016-01-14 18:44:31 +0530

    Update string2id.js

--- string2id.js
+++ string2id.js
@@ -2,7 +2,7 @@ var convertObj = new function() {
 	var textDig = '0123456789abcdfghjkmnpqrstvwxyzABCDFGHJKLMNPQRSTVWXYZ-_',
 		getLen = textDig.length;
 
-		this.decode = function(id) {
+		this.decode = function(id) { // function to convert id into string
 			var node = '';
 			while (id > 0) {
 				node = textDig.charAt(id % getLen) + node;
@@ -11,7 +11,7 @@ var convertObj = new function() {
 			return node;
 		};
 
-		this.decode = function(alpha) {
+		this.decode = function(alpha) { // function to convert strig into id
 			var id = 0;
 			for (var indexNode = 0; indexNode < alpha.length; indexNode++) {
 				id = id * getLen + textDig.indexOf(alpha.charAt(indexNode));
